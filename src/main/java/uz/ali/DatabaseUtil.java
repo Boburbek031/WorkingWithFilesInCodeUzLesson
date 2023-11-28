5cbc472a5f3324df115ad36436ff79aef2867d6c
Boburjon <raximovboburjon66@gmail.com>
2023-11-28 13:04:50 +0500

    first commit in the project of saving files

--- src/main/java/uz/ali/DatabaseUtil.java
+++ src/main/java/uz/ali/DatabaseUtil.java
@@ -9,7 +9,7 @@ public class DatabaseUtil {
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/jbdc_lesson_database",
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/file_database_for_learning_lessons_in_code_uz",
                     "db_user_of_jdbc_lesson", "jdbc0661");
         } catch (SQLException e) {
             throw new RuntimeException(e);
