1dc208654d6386114c8e54f154214ed9fde9c58d
Boburjon <raximovboburjon66@gmail.com>
2023-11-28 14:53:46 +0500

    saving and retrieving images to database and from database in the project of saving files

---
+++ src/main/java/uz/ali/SaveAndGetFilesToDatabase.java
@@ -0,0 +1,66 @@
+package uz.ali;
+
+import java.io.*;
+import java.sql.*;
+
+public class SaveAndGetFilesToDatabase {
+    public static void main(String[] args) {
+
+//        insertImage(new File("src/main/resources/filesThatGoToTheDatabase/img.png"));
+        retrieveImage();
+
+    }
+
+    public static void insertImage(File file) {
+        try (Connection con = DatabaseUtil.getConnection()) {
+            // save image
+//             file = new File("src/main/resources/files/img.png");
+            FileInputStream fis = new FileInputStream(file);
+            String sql = "insert into image_attach (f_name, f_type, f_data) values(?,?,?)";
+
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+
+            preparedStatement.setString(1, "2 - Image");
+            preparedStatement.setString(2, "png");
+            preparedStatement.setBinaryStream(3, fis); // fis ==> file input stream
+            preparedStatement.executeUpdate(); // Insert qilganimiz uchun executeUpdate() ni ishlatamiz.
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void retrieveImage() {
+        try (Connection connection = DatabaseUtil.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select f_name, f_type, f_data from image_attach");
+            while (resultSet.next()) {
+                // Bu 1 - step, database dan file ni yoki filelarni olib unlarni byte ko'rinishiga olib keldik
+                String fName = resultSet.getString("f_name");
+                String fType = resultSet.getString("f_type");
+                InputStream inputStream = resultSet.getBinaryStream("f_data");
+
+                byte[] bytes_of_file = new byte[inputStream.available()];
+                inputStream.read(bytes_of_file);
+
+                // 2 - step da byte ni olib kelgan file type bilan bir xil bo'lgan bitta file ochamiz fa o'sha
+                // file ga byte ni yozib saqlab qo'yamiz va ish bitdi.
+                File file = new File("src/main/resources/filesThatComeFromDatabase/"
+                        + fName + "_come_from_db" + "." + fType);
+                // src/main/resources/filesThatComeFromDatabase/img_1__come_from_db.png // fileni yaratib oldik
+                OutputStream outputStream = new FileOutputStream(file);
+                outputStream.write(bytes_of_file);
+                outputStream.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
